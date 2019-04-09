7ee17903f41485d468d366587d15a7d81951cf10
Steve Regester <sregester@synack.com>
2019-04-08 18:26:23 -0700

    Include more data

--- eighchan-post.js
+++ eighchan-post.js
@@ -2,7 +2,22 @@
 
 const cheerio = require('cheerio')
 
-require('stdin')(htmlInput => {
-  const $ = cheerio.load(htmlInput)
-  console.log($('.intro>a[href]:last-of-type').attr('href'))
+require('stdin')(post_source => {
+  const $ = cheerio.load(post_source)
+
+  const post_uri = $('.intro>a[href]:last-of-type').attr('href')
+  const post_content = $('.body').text()
+  const post_timestamp $('time').attr('unixtime')
+  const poster_id  = $('span.poster_id').text()
+  const poster_name = $('span.name').text()
+
+  console.log(JSON.stringify({
+    post_uri,
+    post_source,
+    post_content,
+    post_timestamp,
+    poster_id,
+    poster_name
+  }, null, 2))
+
 })
